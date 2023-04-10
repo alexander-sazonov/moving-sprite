545e231991c46f013caaaabd712b9183d9aaf0e0
alexander sazonov <alexandersznov@gmail.com>
2023-04-10 16:03:11 +0300

    initial-commit

---
+++ src/Bullet.java
@@ -0,0 +1,5 @@
+public class Bullet extends Sprite{
+    public Bullet(int x, int y) {
+        super(x, y);
+    }
+}
---
+++ src/SpaceShip.java
@@ -0,0 +1,60 @@
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SpaceShip extends Sprite{
+    private int dx;
+    private int dy;
+    List<Bullet> bullets;
+
+    public SpaceShip(int x, int y) {
+        super(x, y);
+        loadImage("space.png");
+        getImageDimensions();
+        bullets = new ArrayList<>();
+    }
+
+    public void move(){
+        x += dx;
+        y += dy;
+    }
+
+    public void fire(){
+
+    }
+
+    public void keyPressed(KeyEvent e){
+        int key = e.getKeyCode();
+        if (key == KeyEvent.VK_LEFT){
+            dx = -1;
+        }
+        if (key == KeyEvent.VK_RIGHT){
+            dx = 1;
+        }
+        if (key == KeyEvent.VK_UP){
+            dy = 1;
+        }
+        if (key == KeyEvent.VK_DOWN){
+            dy = -1;
+        }
+        if (key == KeyEvent.VK_SPACE){
+            fire();
+        }
+
+    }
+    public void keyReleased(KeyEvent e){
+        int key = e.getKeyCode();
+        if (key == KeyEvent.VK_LEFT){
+            dx = 0;
+        }
+        if (key == KeyEvent.VK_RIGHT){
+            dx = 0;
+        }
+        if (key == KeyEvent.VK_UP){
+            dy = 0;
+        }
+        if (key == KeyEvent.VK_DOWN){
+            dy = 0;
+        }
+    }
+}
